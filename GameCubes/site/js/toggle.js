a6341c6960da88db4f6197c4d80f0f1db795e563
Daniel <Daniel.kull@itggot.se>
2018-05-04 21:16:17 +0200

    Removed old project

--- GameCubes/site/js/toggle.js
+++ GameCubes/site/js/toggle.js
@@ -1,10 +1,22 @@
 console.log("Script is running");
 
-// Converts the width and height of the window from 100vw and 100vh to pixels.
-var element = document.querySelector(".all");
-var width = element.clientWidth;
-var height = element.clientHeight;
-element.style.width = width + "px";
-element.style.height = height + "px";
+// // Converts the width and height of the window from 100vw and 100vh to pixels.
+// var element = document.querySelector(".all");
+// var width = element.clientWidth;
+// var height = element.clientHeight;
+// element.style.width = width + "px";
+// element.style.height = height + "px";
 
-console.log(width + ", " + height);
+// console.log(width + ", " + height);
+
+function toggleSidebar() {
+    var element = document.querySelector(".sidebar");
+    element.classList.toggle("show");
+    element.classList.toggle("sidebar_anim");
+}
+
+function toggleOverlay() {
+    var element = document.querySelector(".overlay");
+    element.classList.toggle("show");
+    element.classList.toggle("overlay_anim");
+}
--- GameCubes_dev/site/controller.rb
+++
@@ -1,9 +0,0 @@
-ignore (/\/_.*/)
-ignore (/.*.sass-cache.*/)
-layout ('layout.html.erb')
-ignore ('minify.rb')
-
-before 'main_page.html.erb' do
-    @title = "Gaming time"
-end
-
